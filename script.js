9058432c2d8810f99cb1fbf9d01102f31f77aa44
EC2 Default User <ec2-user@ip-172-31-46-93.us-east-2.compute.internal>
2022-05-02 05:25:28 +0000

    second commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const symbol = require('/node_modules/symbol-sdk')
+
+const addres = symbol.Address.createFromRawAddress("TB7LJMKZNAPLRNCZEMLH6LZZVBXID4G7F3YTCNY")
+
+console.log("hellp symbol")
+
+console.log('Your Address : ${address.plain()}')
