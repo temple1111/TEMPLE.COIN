6fde2b38705f42af511d3ca7968d2e825781cdc5
EC2 Default User <ec2-user@ip-172-31-46-93.us-east-2.compute.internal>
2022-05-03 22:49:55 +0000

    5/4-07

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const symbol = require('/node_modules/symbol-sdk')
 
 const GENERATION_HASH = '7FCCD304802016BEBBCD342A332F91FF1F3BB5E902988B352697BE245F48E836'
 const EPOCH = 1637848847
-const XYM_ID = '152F416ABD3AB018'
+const XYM_ID = '3A8416DB2D53B6C8'
 const NODE_URL = 'https://sym-test.opening-line.jp:3001'
 const NET_TYPE = symbol.NetworkType.TEST_NET
 
@@ -23,7 +23,7 @@ accountHttp.getAccountInfo(address)
     for (let m of accountInfo.mosaics) {
       if (m.id.id.toHex() === XYM_ID) {
         const dom_xym = document.getElementById('wallet-xym')
-        dom_xym.innerText = `TEMPLE point : ${m.amount.compact() / Math.pow(10, 6)}`
+        dom_xym.innerText = `XYM 残高 : ${m.amount.compact() / Math.pow(10, 6)}`
       }
     }
   })
