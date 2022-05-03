d4ee5c1fe1e2bda63a720ee5f1a4f5a9d921c6e5
EC2 Default User <ec2-user@ip-172-31-46-93.us-east-2.compute.internal>
2022-05-03 01:25:14 +0000

    5/3-3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const symbol = require('/node_modules/symbol-sdk')
 
 const GENERATION_HASH = '7FCCD304802016BEBBCD342A332F91FF1F3BB5E902988B352697BE245F48E836'
 const EPOCH = 1637848847
-const XYM_ID = '3A8416DB2D53B6C8'
+const XYM_ID = '152F416ABD3AB018'
 const NODE_URL = 'https://sym-test.opening-line.jp:3001'
 const NET_TYPE = symbol.NetworkType.TEST_NET
 
